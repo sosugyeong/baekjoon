342aa325574ad4553023b2ed182cbc5597be3b19
sosugyeong <99548138+sosugyeong@users.noreply.github.com>
2024-04-30 23:09:48 +0900

    [Bronze I] Title: 소인수분해, Time: 148 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	
+	public static void main(String[] args) throws IOException
+    {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 2; i<=Math.sqrt(n); i++) {
+			while(n%i == 0) {
+				sb.append(i+"\n");
+				n /= i;
+			}
+		}
+		if(n != 1) {
+			sb.append(n);
+		}
+		System.out.println(sb);
+        br.close();
+    }
+}
