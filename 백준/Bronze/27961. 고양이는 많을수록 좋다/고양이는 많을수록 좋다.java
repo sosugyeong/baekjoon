73e28d7773fc8eb0b3a36721c0c4a1b8d1b968b8
Sugyeong <99548138+sosugyeong@users.noreply.github.com>
2024-11-09 21:57:13 +0900

    [Bronze I] Title: 고양이는 많을수록 좋다, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(br.readLine());
+        long cat = 1L;
+        long num = 1L;
+
+        if(N == 0){
+            num = 0;
+        } else {
+            for(long i = 1L; i<N; i++){
+                cat = cat*2;
+
+                if(N<=cat){
+                    num = i+1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(num);
+    }
+}
